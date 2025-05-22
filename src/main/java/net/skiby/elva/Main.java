5b21f6b8dbaf8975f364248d7caffaad1c23af9a
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2025-05-23 01:42:31 +0200

    added support for float number tokenization

--- src/main/java/net/skiby/elva/Main.java
+++ src/main/java/net/skiby/elva/Main.java
@@ -7,10 +7,14 @@ public class Main {
     public static void main(String[] args) {
         var t = new Tokenizer();
 
-        var tokens = t.tokenize("This is a test text. 12345 something");
+        var input = "var x = 555,5555 * 5,5;";
+        var tokens = t.tokenize(input);
 
         for (Token token : tokens) {
             System.out.println(token.toString());
         }
+
+        System.out.println(input);
+        System.out.println(Tokenizer.reconstruct(tokens));
     }
 }
--- src/main/java/net/skiby/elva/tokenizer/Tokenizer.java
+++ src/main/java/net/skiby/elva/tokenizer/Tokenizer.java
@@ -2,9 +2,10 @@ package net.skiby.elva.tokenizer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Tokenizer {
-    private String buffer = "";
+    private StringBuilder buffer = new StringBuilder();
     private final List<Token> tokens = new ArrayList<>();
     private TokenType suspectedTokenType;
     private int bufferStartPosition;
@@ -27,13 +28,17 @@ public class Tokenizer {
             suspectedTokenType = identifyTokenType(c);
         }
 
-        buffer += c;
+        buffer.append(c);
 
         var nextPosition = currentPosition + 1;
         if (nextPosition < input.length()) {
             var nextCharacter = input.charAt(nextPosition);
 
             if (suspectedTokenType != identifyTokenType(nextCharacter)) {
+                if (wouldBeValidFloat(c, nextCharacter)) {
+                    return;
+                }
+
                 tokenizeBuffer();
 
                 suspectedTokenType = identifyTokenType(nextCharacter);
@@ -45,9 +50,9 @@ public class Tokenizer {
     }
 
     private void tokenizeBuffer() {
-        final var token = new Token(buffer, suspectedTokenType, bufferStartPosition + 1, currentPosition + 1);
+        final var token = new Token(buffer.toString(), suspectedTokenType, bufferStartPosition + 1, currentPosition + 1);
         tokens.add(token);
-        buffer = "";
+        buffer.setLength(0);
     }
 
     private TokenType identifyTokenType(char c) {
@@ -57,8 +62,37 @@ public class Tokenizer {
             return TokenType.NUMBER;
         } else if (Character.isLetter(c)) {
             return TokenType.IDENTIFIER;
+        } else if (isSymbol(c)) {
+            return TokenType.SYMBOL;
         }
 
         return TokenType.UNKNOWN;
+    }
+
+    private boolean isSymbol(char c) {
+        var symbols = Set.of('+', '-', '*', '/', ',');
+        return symbols.contains(c);
+    }
+
+    private boolean wouldBeValidFloat(char current, char next) {
+        if (buffer.indexOf(",") != -1) {
+            return false;
+        }
+
+        if (currentPosition + 2 >= input.length()) {
+            return false;
+        }
+
+        return Character.isDigit(current) && next == ',' && Character.isDigit(input.charAt(currentPosition+2));
+    }
+
+    public static String reconstruct(List<Token> tokens) {
+        var output = new StringBuilder();
+
+        for (Token token : tokens) {
+            output.append(token.value);
+        }
+
+        return output.toString();
     }
 }
