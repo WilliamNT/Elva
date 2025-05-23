52c3b69e2a7db675e4b4bf8ca828af21e4ccc253
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2025-05-23 14:53:27 +0200

    made buffer final

--- src/main/java/net/skiby/elva/tokenizer/Tokenizer.java
+++ src/main/java/net/skiby/elva/tokenizer/Tokenizer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tokenizer {
-    private StringBuilder buffer = new StringBuilder();
+    private final StringBuilder buffer = new StringBuilder();
     private final List<Token> tokens = new ArrayList<>();
     private TokenType suspectedTokenType;
     private int bufferStartPosition;
